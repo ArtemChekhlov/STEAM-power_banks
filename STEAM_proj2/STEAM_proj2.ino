6da940ceee2134879bc9488230dc2064aaee2b70
Shareneon <e.brusov222@gmail.com>
2026-01-14 19:55:51 +0300

    Update STEAM_proj2.ino

--- STEAM_proj2/STEAM_proj2.ino
+++ STEAM_proj2/STEAM_proj2.ino
@@ -1,6 +1,8 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Keypad.h>
+// Настройка пинов для SD модуля
+#define SD_CS_PIN 4  // Chip Select пин для SD карты
 
 struct Entry {
   String key1;  // основной ключ
@@ -396,6 +398,7 @@ bool checkPasswordInFileWithPasswords(String passwordToCheck) {
       break;  // Пароль найден, выходим из цикла
     }
   }
+  passwordFile.close();
 }
 
 // ---------- SETUP / LOOP ----------
